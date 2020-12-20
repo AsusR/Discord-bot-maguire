72ed8c47af69332d8f70d41af1ddcadbcc42bbb7
AsusR <rehansiddiqui44@gmail.com>
2020-12-20 11:47:19 +0530

    fixed something again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const config = require('./config.json');
-const commands = require('./command')
+const command = require('./command')
 
 client.on('ready', () => {
     console.log('The Client is ready');
