2869abe00a4580a5c150682b72bbe4de41979eee
AsusR <rehansiddiqui44@gmail.com>
2020-12-20 21:02:43 +0530

    commented all commands so it doesnt bug till i find a fix

--- index.js
+++ index.js
@@ -10,42 +10,45 @@ client.on('ready', () => {
     console.log('The Client is ready');
 
     //Command 1
-    command(client, ['ping', 'test', 'hello'], message => {
-        message.channel.send('Pong!');
-    });
+    // command(client, ['ping', 'test', 'hello'], message => {
+    //     message.channel.send('Pong!');
+    // });
 
-    //Command 2
-    command(client, 'servers', message => {
-        client.guilds.cache.forEach(guild => {
-            message.channel.send(`${guild.name} has total of ${guild.memberCount} members`)
-        });
-    });
+    // //Command 2
+    // command(client, 'servers', message => {
+    //     client.guilds.cache.forEach(guild => {
+    //         message.channel.send(`${guild.name} has total of ${guild.memberCount} members`)
+    //     });
+    // });
 
-    //Command 3 
-    command(client, ['cc', 'clearchannel'], message => {
-        if (message.member.hasPermission('ADMINISTRATOR')) {
-            message.channel.messages.fetch().then(results => {
-                message.channel.bulkDelete(results)
-             });
-            };
-        });
+    // //Command 3 
+    // command(client, ['cc', 'clearchannel'], message => {
+    //     if (message.member.hasPermission('ADMINISTRATOR')) {
+    //         message.channel.messages.fetch().then(results => {
+    //             message.channel.bulkDelete(results)
+    //          });
+    //         };
+    //     });
 
-        //Command 4
-        command(client, 'status', message => {
-            const content = message.content.replace('!status ', '');
-            // "!status something" if didnt replace status becomes "!status something" using replace it becomes "something"
+    //     //Command 4
+    //     command(client, 'status', message => {
+    //         const content = message.content.replace('!status ', '');
+    //         // "!status something" if didnt replace status becomes "!status something" using replace it becomes "something"
        
-            client.user.setPresence({
-                activity: {
-                    name: content,
-                    type: 0
-                }
-            })
-        });
-        // Sends a message on the channel if exist edits it else sends a new message
+    //         client.user.setPresence({
+    //             activity: {
+    //                 name: content,
+    //                 type: 0
+    //             }
+    //         })
+    //     });
+    //     // Sends a message on the channel if exist edits it else sends a new message
        // firstMessage(client, '789889036562726922', 'hello world!!!', ['🔥', '🚒'])
         
         privateMessage(client, 'ping', 'Pong!');
+        client.users.fetch('352385166410514434').then(user => {
+            user.send('You have summoned me');
+        })
     });
 
 
