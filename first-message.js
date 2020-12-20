d54ff9079e8cb5dc4d25b3eb41c9191d11186428
AsusR <rehansiddiqui44@gmail.com>
2020-12-20 13:02:40 +0530

    added async which i forgo

--- first-message.js
+++ first-message.js
@@ -6,7 +6,7 @@ const addReactions = (message, reactions) => {
     }
 }
 
-module.exports = (cliet, id, text, reactions = []) => {
+module.exports = async (cliet, id, text, reactions = []) => {
     const channel = await client.channels.fetch(id)
     channel.messages,fetch().then((messages) => {
         if(messages.size === 0){
