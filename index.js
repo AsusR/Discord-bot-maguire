2110102d0c3469387cf321414dd674d35f856bf6
AsusR <rehansiddiqui44@gmail.com>
2020-12-20 11:24:28 +0530

    updated to config.json not ENV

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-//const config = require('./config.json');
+const config = require('./config.json');
 
 client.on('ready', () => {
     console.log('The Client is ready');
 });
 
-client.login(process.env.MAGUIREBOT);
+client.login(config.token);
