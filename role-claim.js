214d4e5b7e8ee776b01389c4a7379ee301bd8665
AsusR <rehansiddiqui44@gmail.com>
2020-12-21 17:20:55 +0530

    added role selection

--- role-claim.js
+++ role-claim.js
@@ -2,5 +2,27 @@ const firstMessage = require('./first-message')
 module.exports = client => {
     const channelId = '790542617912803329';
 
-    firstMessage(client, channelId, 'hello world', []);
+    const getEmoji = emojiName => {
+        client.emojis.cache.find(emoji => {
+            emoji.name === emojiName
+        })
+    }
+    const emojis = {
+        role1 : 'Role 1',
+        role2 : 'Role 2',
+        role3 : 'Role 3'
+    }
+
+    const reactions = []
+
+    let emojiText = 'Select Role by reacting to the emotes';
+    for (const key in emojis) {
+        const emoji = getEmoji(key);
+        reactions.push(emoji);
+
+        const role = emojis[key];
+        emojiText += `${emoji} = ${role}\n`
+    }
+
+    firstMessage(client, channelId, emojiText, []);
 }
