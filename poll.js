6617f242c0e8884fb78032b9deb192d95c26c4f4
AsusR <rehansiddiqui44@gmail.com>
2020-12-22 15:36:40 +0530

    added poll

--- poll.js
+++ poll.js
@@ -1,6 +1,6 @@
 module.exports = client => {
     const channelIds = [
-        '790875077251301377' // poll-suggestions 
+       '790875077251301377' // poll-suggestions 
     ]
 
     const addReactions = message => {
@@ -11,9 +11,16 @@ module.exports = client => {
         }, 750)
     }
 
-    client.on('message', message => {
+    client.on('message', async (message) => {
         if (channelIds.includes(message.channel.id)){
             addReactions(message)
+        } else if(!channelIds && message.content.toLowerCase() === '!poll') {
+            await message.delete()
+
+            const fetched = await message.channel.messages.fetch({ limit: 1 })
+            if(fetched && fetched.first()){
+                addReactions(fetched.first());
+            }
         }
-    })
+    });
 }
